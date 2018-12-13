c9febe70c07f34d6eae013e7bdfc46698d94810c
Sergey Gladkovskiy <smgladkovskiy@gmail.com>
2018-12-13 10:10:33 +0300

    mux response error

--- mux.go
+++ mux.go
@@ -1,10 +1,15 @@
 package errors
 
 import (
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	"github.com/sirupsen/logrus"
+	"gitlab.teamc.io/teamc.io/microservice/support/logs-go.git"
+	"gopkg.in/go-playground/validator.v9"
 	"net/http"
 )
 
-type Response struct {
+type ResponseObject struct {
 	Error ErrorObject `json:"error"`
 }
 
@@ -13,6 +18,67 @@ type ErrorObject struct {
 	Code       int                 `json:"code,omitempty"`
 	Validation map[string][]string `json:"validation,omitempty"`
 	Debug      string              `json:"debug,omitempty"`
+}
+
+func Response(c *gin.Context, err interface{}) {
+	var (
+		jsonObj *ResponseObject
+	)
+	errCode := http.StatusBadRequest
+
+	switch et := err.(type) {
+	case gorm.Errors:
+		if gorm.IsRecordNotFoundError(et) {
+			errCode = http.StatusNotFound
+		} else {
+			errCode = http.StatusBadRequest
+		}
+
+		jsonObj = &ResponseObject{Error: ErrorObject{
+			Message: et.Error(),
+		}}
+
+		logs.DBLogs.WithError(et).WithField("stage", "query").Error("Query error occurred")
+
+	case AppError:
+		errCode = et.Code
+
+		jsonObj = &ResponseObject{Error: ErrorObject{Message: et.Error()}}
+
+		logs.Log.
+			WithError(et).
+			WithFields(logrus.Fields{
+				"message": et.Error(),
+			}).
+			Error("application error occurred")
+	case validator.ValidationErrors:
+		errCode = http.StatusUnprocessableEntity
+
+		jsonObj = &ResponseObject{Error: ErrorObject{
+			Message:    "Ошибка валидации",
+			Validation: MakeErrorsSlice(et),
+		}}
+
+		logs.HttpLogs.
+			WithError(et).
+			WithFields(logrus.Fields{
+				"validation": MakeErrorsSlice(et),
+			}).
+			Warn("validation error occurred")
+	case error:
+		errCode = getErrCode(et)
+
+		jsonObj = &ResponseObject{Error: ErrorObject{Message: et.Error()}}
+
+		logs.Log.
+			WithError(et).
+			WithFields(logrus.Fields{
+				"message": et.Error(),
+			}).
+			Warn("error occurred")
+	}
+
+	c.AbortWithStatusJSON(errCode, jsonObj)
 }
 
 func getErrCode(et error) (errCode int) {
