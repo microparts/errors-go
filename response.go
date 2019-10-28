2999a1e7a3c75a29ea6b639948834e18955746dc
Sergey Gladkovskiy <smgladkovskiy@gmail.com>
2019-10-28 16:32:19 +0300

    adding optional error type to error object

--- response.go
+++ response.go
@@ -3,14 +3,22 @@ package errors
 type FieldName string
 type ValidationError string
 type ErrorCode string
+type ErrorType string
 type DebugData interface{}
 
 type Response struct {
 	Error ErrorObject `json:"error,omitempty"`
 }
 
+const (
+	ErrorTypeError   ErrorType = "error"
+	ErrorTypeWarning ErrorType = "warning"
+	ErrorTypeInfo    ErrorType = "info"
+)
+
 type ErrorObject struct {
 	Message    interface{}                     `json:"message"`
+	Type       *ErrorType                      `json:"type,omitempty"`
 	Code       *ErrorCode                      `json:"code,omitempty"`
 	Validation map[FieldName][]ValidationError `json:"validation,omitempty"`
 	Debug      *DebugData                      `json:"debug,omitempty"`
